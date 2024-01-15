3286df5abac04bc3bdfe385d3c6477540aab3b1b
Omkar Dhanave <o.m.dhanave@gmail.com>
2024-01-15 16:31:18 +0530

    add

--- server.js
+++ server.js
@@ -2,31 +2,90 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const PDFDocument = require('pdfkit');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(cors()); // Enable CORS for all routes
+
+app.use(cors());
+app.use(express.json());
+
 // MongoDB connection
 mongoose.connect('mongodb+srv://omkardhanave:omkar@cluster0.liqetg2.mongodb.net/SamplePDF?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Create mongoose model for storing data
+// Create mongoose model for storing user data
 const User = mongoose.model('User', {
-  name: String,
-  photo: String,
+  username: String,
+  password: String,
 });
 
 // Multer setup for handling file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.use(express.json());
+// Express session setup
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// Middleware to check user authentication
+const authenticateUser = (req, res, next) => {
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+};
+
+// User registration endpoint
+app.post('/api/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Hash the password before saving it to the database
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+
+  res.status(201).send('User registered successfully');
+});
+
+// User login endpoint
+app.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (user && await bcrypt.compare(password, user.password)) {
+    // Create a token and store it in a cookie
+    const token = jwt.sign({ username: user.username }, 'your-secret-key');
+    req.session.user = user;
+    res.cookie('token', token, { httpOnly: true });
+
+    res.status(200).send('Login successful');
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+// Logout endpoint
+app.post('/api/logout', (req, res) => {
+  req.session.destroy();
+  res.clearCookie('token');
+  res.status(200).send('Logout successful');
+});
 
 // API endpoint for saving user data
 // API endpoint for saving user data
-app.post('/api/users', upload.single('photo'), async (req, res) => {
+app.post('/api/users',authenticateUser, upload.single('photo'), async (req, res) => {
   try {
     const { name } = req.body;
 
